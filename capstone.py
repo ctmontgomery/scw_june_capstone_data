2b580f10d241c11e9483b2161b58bdf2838d8b21
ctmontgomery <cmontgomery@lsst.org>
2016-06-12 16:40:28 -0700

    Added code to plot pop data

--- capstone.py
+++ capstone.py
@@ -5,3 +5,12 @@ Chris and Swapnil
 import pandas as pd
 from matplotlib import pyplot as plt
 dat = pd.read_csv("data/gapminder-FiveYearData.csv")
+
+import pandas as pd
+from matplotlib import pyplot as plt
+dat = pd.read_csv("data/gapminder-FiveYearData.csv")
+dat.head()
+zim_dat = dat[dat['country'] == "Zimbabwe"]
+zim_dat.plot (x='year', y='lifeExp')
+zim_dat.hist()
+
